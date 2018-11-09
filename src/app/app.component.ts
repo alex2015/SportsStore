1153439457f8b136ce14c589de01755a382bf475
alex2015 <alex2015@rambler.ru>
2018-11-09 03:25:11 +0300

    step 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div class="bg-success p-a-1 text-xs-center">This is SportsStore</div>`
 })
 export class AppComponent {
   title = 'app';
