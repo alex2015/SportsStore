de4816b1bce0169db29c3e37df1a7f89bbd62380
alex2015 <alex2015@rambler.ru>
2018-11-11 01:35:45 +0300

    step 9

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -12,30 +12,56 @@ const PORT = 3500;
 @Injectable()
 export class RestDataSource {
   baseUrl: string;
+  auth_token: string;
+
   constructor(private http: Http) {
     this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+  }
+
+  authenticate(user: string, pass: string): Observable<boolean> {
+    return this.http.request(new Request({
+      method: RequestMethod.Post,
+      url: this.baseUrl + 'login',
+      body: { name: user, password: pass }
+    })).map(response => {
+      const r = response.json();
+      this.auth_token = r.success ? r.token : null;
+      return r.success;
+    });
   }
 
   getProducts(): Observable<Product[]> {
     return this.sendRequestProducts(RequestMethod.Get, 'products');
   }
 
-  private sendRequestProducts(verb: RequestMethod, url: string): Observable<Product[]> {
-    return this.http.request(new Request({
+  private sendRequestProducts(verb: RequestMethod, url: string, auth: boolean = false): Observable<Product[]> {
+    const request = new Request({
       method: verb,
-      url: this.baseUrl + url,
-    })).map(response => response.json());
+      url: this.baseUrl + url
+    });
+
+    if (auth && this.auth_token != null) {
+      request.headers.set('Authorization', 'Bearer<${this.auth_token}>');
+    }
+
+    return this.http.request(request).map(response => response.json());
   }
 
   saveOrder(order: Order): Observable<Order> {
     return this.sendRequestOrder(RequestMethod.Post, 'orders', order);
   }
 
-  private sendRequestOrder(verb: RequestMethod, url: string, body: Order): Observable<Order> {
-    return this.http.request(new Request({
+  private sendRequestOrder(verb: RequestMethod, url: string, body: Order, auth: boolean = false): Observable<Order> {
+    const request = new Request({
       method: verb,
       url: this.baseUrl + url,
       body: body
-    })).map(response => response.json());
+    });
+
+    if (auth && this.auth_token != null) {
+      request.headers.set('Authorization', 'Bearer<${this.auth_token}>');
+    }
+
+    return this.http.request(request).map(response => response.json());
   }
 }
