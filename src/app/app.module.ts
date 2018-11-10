503ce2ec2397f1103616c817fb6f5e5d4dfd1b24
alex2015 <alex2015@rambler.ru>
2018-11-10 16:22:40 +0300

    step 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from 'app/store/store.module';
+import { StoreComponent } from './store/store.component';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { RouterModule } from '@angular/router';
+import { StoreFirstGuard } from './storeFirst.guard';
 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    StoreModule
-  ],
-  providers: [],
+  imports: [BrowserModule, StoreModule,
+    RouterModule.forRoot([
+      { path: 'store', component: StoreComponent, canActivate: [StoreFirstGuard] },
+      { path: 'cart', component: CartDetailComponent, canActivate: [StoreFirstGuard] },
+      { path: 'checkout', component: CheckoutComponent, canActivate: [StoreFirstGuard] },
+      { path: '**', redirectTo: '/store' }
+    ])],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
